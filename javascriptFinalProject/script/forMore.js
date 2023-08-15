a247ece6916e753d74f285ffed2106000b3f0a72
nickshakh <shakhbazyann@gmail.com>
2023-08-16 00:54:09 +0400

    ,

---
+++ javascriptFinalProject/script/forMore.js
@@ -0,0 +1,1 @@
+import {createNode,append } from "./script1";
--- javascriptFinalProject/script/script1.js
+++ javascriptFinalProject/script/script1.js
@@ -1,4 +1,5 @@
 'use strict'
+export {createNode,append}
 
 const url = 'https://dummyjson.com/products'
 const ul = document.getElementById('authors');
@@ -23,22 +24,24 @@ fetch(url)
     let shopProducts = data.products
     console.log(data.products)
     return shopProducts.map(function(product) {
-        let a = createNode('a')
         let p = createNode('p')
+        let a = createNode('a')
         let img1 = createNode('img')
-        a.classList.add('col')
+        a.classList.add('row')
         p.classList.add('col')
         img1.classList.add('col')
         img1.src = product.thumbnail;
         // h1.innerHTML= `${product.title}`
 
-        a.innerHTML = `Click here for more info `
+        a.textContent = `Click here for more info `
+        a.href = `./index2.html`
         p.innerHTML = `<h1>${product.title}</h1> <b>Rating :</b> <i>${product.rating}</i> / 5 <br> <br> <b> Product price </b> : ${product.price} usd <br> <b>Product Description</b> ${product.description}`
-        a.style.fontSize = '25px'
+        a.style.fontSize = '20px'
+        p.style.width = '20px'
         // container.style.margin = '180px'
        const imgAppend = append(ulCont,img1)
+       const pAppend = append (ulCont,p)
         const aAppend = append(ulCont,a)
-        const pAppend = append (ulCont,p)
     })
     .catch(function(error) {
         console.log(error);
